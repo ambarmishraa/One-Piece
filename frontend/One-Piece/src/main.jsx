2508cdbf40ea376f86b6b80e08d33feab8fd80c6
ambarmishraa <ambarmishra740@gmail.com>
2025-07-24 08:05:40 +0530

    updated the structure

--- frontend/One-Piece/src/main.jsx
+++ frontend/One-Piece/src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './index.jsx'
 import './index.css';
 import SignUp from  './user/pages/signUp.jsx'
-import Error from './404.jsx'
+import Error from './error.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    {/* <SignUp /> */}
-    <Error />
+    <SignUp />
+    {/* <Error /> */}
   </StrictMode>,
 )
