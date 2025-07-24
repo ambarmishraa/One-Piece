d6574199d910cab14d19b77d9e9b9a1c2ed4209a
ambarmishraa <ambarmishra740@gmail.com>
2025-07-24 23:09:21 +0530

    bycrpt js insatlled and implemented on the password hashing

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
     {
@@ -15,7 +16,15 @@ const userSchema = new mongoose.Schema(
     {
         timestamps: true,
     }
-)
+);
+
+// 🔐 Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next(); // only hash if changed
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
