4e5177b5811800a3d3e147a8e48d184a92f5b2a9
ambarmishraa <ambarmishra740@gmail.com>
2025-07-24 23:10:40 +0530

    added the hashing code in admin aslo

--- backend/model/adminModel.js
+++ backend/model/adminModel.js
@@ -17,6 +17,14 @@ const adminSchema = new mongoose.Schema(
     }
 )
 
+// 🔐 Hash password before saving
+adminSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next(); // only hash if changed
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
 const Admin = mongoose.model("Admin", adminSchema);
 
 module.exports = Admin;
