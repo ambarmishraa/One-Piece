67af35ee5339290268084bac915548c1be9fe059
ambarmishraa <ambarmishra740@gmail.com>
2025-07-24 06:42:23 +0530

    created login and signup page

--- frontend/One-Piece/src/user/pages/signIn.jsx
+++ frontend/One-Piece/src/user/pages/signIn.jsx
@@ -1,19 +1,30 @@
 const SignIn = () => {
     return (
         <>
-            <div>
-                <h1 className="font-bold" >Sign In Page</h1>
-                <input className="p-2" type="email" placeholder="Enter your Email" required />
-                <input className="p-2" type="password" placeholder="Enter your password" required />
-                <a
-                    href="/SignIn"
-                    className="bg-green-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded"
-                >
-                    SignIn
-                </a>
+            <div className="auth-wrapper flex items-center justify-center overflow-hidden relative">
+                <form action="" method="POST">
+                    <div className="w-[500px] h-[500px] backdrop-blur-md bg-black/10 border border-white/50 rounded-xl p-6 shadow-lg flex font-bold justify-center">
+                        <div className=" w-[100%] h-[100%] ">
+                            <div className="flex justify-center mb-4">
+                                <p className="text-white text-3xl">LOGIN</p>
+                            </div>
+                            <div className="flex justify-center">
+                                <input className="p-2 w-[80%] text-white mb-4 placeholder-white border-white/40 backdrop-blur-md rounded shadow-lg" type="email" placeholder="Email" required />
+                            </div>
+                            <div className="flex justify-center">
+                                <input className="p-2 mb-4 text-white w-[80%] placeholder-white border-white/40 backdrop-blur-md rounded shadow-lg" type="password" placeholder="Password" required />
+                            </div>
+                            <div className="flex justify-end w-[90%]">
+                                <button type="submit"
+                                    className="bg-white w-[35%] cursor-pointer hover:bg-blue-700 text-black font-semibold py-2 px-4 rounded">LOGIN</button>
+                            </div>
+                            <div style={{ backgroundColor: 'transparent', position: 'absolute', bottom: '-1px',  zIndex: -1   }}>
+                                <img src="/itachi.png" style={{width:'370px'}} alt="FormImage" />
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
-            <a href="/SignUp"
-                className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">Create Account</a>
         </>
     )
 }
--- frontend/One-Piece/src/user/pages/signUp.jsx
+++ frontend/One-Piece/src/user/pages/signUp.jsx
@@ -1,19 +1,31 @@
 const SignUp = () => {
     return (
-         <>
-            <div>
-                <h1 className="font-bold" >Sign Up Page</h1>
-                <input className="p-2" type="email" placeholder="Enter your Email" required />
-                <input className="p-2" type="password" placeholder="Enter your password" required />
-                <a
-                    href="/SignUp"
-                    className="bg-green-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded"
-                >
-                    SignUp
-                </a>
+        <>
+            <div className="auth-wrapper flex items-center justify-center overflow-hidden relative">
+                <form action="" method="POST">
+                    <div className="w-[500px] h-[500px] backdrop-blur-md bg-black/10 border border-white/50 rounded-xl p-6 shadow-lg flex font-bold justify-center">
+                        <div className=" w-[100%] h-[100%] ">
+                            <div className="flex justify-center mb-4">
+                                <p className="text-white text-3xl">SIGNUP</p>
+                            </div>
+                            <div className="flex justify-center">
+                                <input className="p-2 w-[80%] placeholder-white mb-4 border-white/40 backdrop-blur-md rounded text-white shadow-lg" type="email" placeholder="Email" required />
+                            </div>
+                            <div className="flex justify-center">
+                                <input className="p-2 mb-4 w-[80%] text-white placeholder-white border-white/40 backdrop-blur-md rounded shadow-lg" type="password" placeholder="Password" required />
+                            </div>
+                            <div className="flex justify-end w-[90%]">
+                                <button type="submit"
+                                    className="bg-white cursor-pointer w-[35%] hover:bg-blue-700 text-black font-semibold py-2 px-4 rounded">REGISTER
+                                </button>
+                            </div>
+                            <div style={{ backgroundColor: 'transparent', position: 'absolute', bottom: '-1px', zIndex: -1 }}>
+                                <img src="/itachi.png" style={{ width: '370px' }} alt="FormImage" />
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
-            <a href="/SignIn"
-                className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">Sign In</a>
         </>
     )
 }
